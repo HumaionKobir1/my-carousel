c0792b93c9ebc74f14ced86ec96de0f8afc9f9e9
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-17 20:19:56 +0600

    Added => javaScript code and Create a simple slider

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg'
+];
+
+let imgIndex = 0;
+const imgEl = document.getElementById('slider-img');
+setInterval(() => {
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    imgEl.setAttribute('src', imgUrl);
+    // console.log(imgUrl);
+    imgIndex++;
+} , 1000);
